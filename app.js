b5adde5ccd7c07849e2de011a23a76fc36805f6d
tabishismail <tabish554@gmail.com>
2021-05-31 20:12:02 +0500

    Assignment # 2 done

--- app.js
+++ app.js
@@ -7,3 +7,12 @@
 //     document.write(time);
 // }
 // date();
+
+// Assignment # 2
+
+// function greeting(){
+//     var firstName= prompt("Enter Your First Name");
+//     var lastName= prompt("Enter your last Name");
+//     alert("Hellow "+ firstName+" "+lastName+ " Welcome to our country");
+// }
+// greeting();
